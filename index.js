86aefe4de14b61e893648345963b11bbd30a9e52
FrancoMurina <Fmurina@udesa.edu.ar>
2020-10-13 10:02:26 -0300

    primera vez

---
+++ index.js
@@ -0,0 +1,18 @@
+
+alert ("Bienvenidos a mi sitio");
+let confirmacion = confirm ("Esta seguro de querer avanzar");
+termina = document.querySelector(".termina");
+if (confirmacion == false) {termina.innerHTML = "Lamentamos que no querias continuar tu visita por este sitio increible."}
+else if (confirmacion == true) {termina.innerHTML = "¡Qué alegría que quieras seguir con tu visita por este maravilloso sitio!";
+nombre = prompt ("¿Cual es su nombre?") ; } 
+saludo = document.querySelector(".sigue");
+saludo.innerHTML = "Bienvendido " + nombre;
+
+edad = prompt("¿Cual es su edad?");
+if(edad <18){acceso= alert("Prohibido")}
+else if (edad>= 18){acceso= alert("Permitido");oscuro = confirm ("¿Desea usted utilizar el modo oscuro?")
+cuerpo = document.querySelector(".cuerpo");
+if (osucro = true) cuerpo.style.backgroundColor = "dimgray"; cuerpo.style.color = "white";
+
+diaDeSemana = ("Domingo")
+if (diaDeSemana = "Domingo"){alert("¡Hoy es día de descanso!")} }
