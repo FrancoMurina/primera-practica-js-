c3edd39e9d1a6e9ac5436cc4c94f326271a686fa
FrancoMurina <Fmurina@udesa.edu.ar>
2020-10-13 19:39:15 -0300

    finalizada la practica

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ saludo = document.querySelector(".sigue");
 saludo.innerHTML = "Bienvendido " + nombre;
 
 edad = prompt("¿Cual es su edad?");
-if(edad <18){acceso= alert("Prohibido su acceso")}
+if(edad <18){acceso= alert("Prohibido su acceso");termina.innerHTML = "Lamentamos que no puedas continuar tu visita por este sitio increible."; saludo.innerHTML = " " }
 else if (edad>= 18){acceso= alert("Permitido su acceso");oscuro = confirm ("¿Desea usted utilizar el modo oscuro?")
 cuerpo = document.querySelector(".cuerpo");
 if (oscuro == true) {cuerpo.style.backgroundColor = "dimgray"; cuerpo.style.color = "white";}
 
 diaDeSemana = ("Domingo")
-if (diaDeSemana = "Domingo"){alert("¡Hoy es día de descanso!")} }
+dia = document.querySelector(".dia");
+if (diaDeSemana = "Domingo"){dia.innerHTML = "¡Hoy es día de descanso!"} }
